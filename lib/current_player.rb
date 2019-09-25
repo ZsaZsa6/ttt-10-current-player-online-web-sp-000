1aee83bb86c017e08720077d3e1f629a6310d8a5
Ronda D Cowen <cowenhome5@gmail.com>
2019-09-25 14:53:53 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,14 +5,5 @@ def turn_count(board)
     if block == "X" || block == "O"
       counter += 1 
    end
-end  
- counter
-end
-
-  def current_player(board)
-    if turn_count(board) % 2 == 0 
-      return "X"
-    else
-      return "O"
-    end
+e
   end
